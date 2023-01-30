d3763f83b97a86750025e9b6713a33dce5386db3
Purval Kamble <purval.kamble@capgemini.com>
2023-01-30 16:11:23 +0530

    Learned the click event and functions in ReactJs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 
 import './App.css';
-import FunctionalCompo from './FunctionalCompo';
-import ClassCompo from './ClassCompo';
+// import FunctionalCompo from './FunctionalCompo';
+// import ClassCompo from './ClassCompo';
 function App() {
+  let courseName = 'React Library';
+  function apple() {
+    courseName = 'Python';
+    alert(courseName);
+  }
   return (
     <div className="App">
-      <h1>Hello World</h1>
-      <FunctionalCompo/>
-      <ClassCompo/>
+      <h1>{courseName}</h1>
+      <button onClick={apple}>Click Me</button>
+      {/* <FunctionalCompo/>
+      <ClassCompo/> */}
     </div>
   );
 }
--- src/FunctionalCompo.js
+++ src/FunctionalCompo.js
@@ -1,6 +1,6 @@
 function FunctionalCompo(){                            //This is the Functional Component Example 
     return(
-        <h1>FunctionalCompo Component</h1>
+        <div>Hello</div>
     )
 };
 
